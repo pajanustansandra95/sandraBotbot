f5cefe0d364b3057a0a91eea7461d31419a6fda2
pajanustansandra95 <pajanustansandra94@gmail.com>
2024-05-29 10:53:50 +0800

    Update stalk.js

--- script/stalk.js
+++ script/stalk.js
@@ -1,51 +1,50 @@
-const axios = require("axios");
-const fs = require('fs')
-module.exports = {
-  config: {
-    name: "stalk" ,
-    usePrefix: true,
-     credits:"1SOY DEV",
-     usage:`stalk @user`,
-    description: "stalk the tag user",
-    permission: 1, 
-  },
-  run: async function({ api, event, args, commandModules }) {
- try{
-if (Object.keys(event.mentions).length === 0) {
-  api.sendMessage('Please Mention A User', event.threadID, event.messageID);
-} else {
-  for (var i = 0; i < Object.keys(event.mentions).length; i++) {
-    const mentionedUID = Object.keys(event.mentions)[i];
-    const userInfo = await api.getUserInfo(mentionedUID);
-    const fname = userInfo[mentionedUID].name;
-const acculr = userInfo[mentionedUID].profileUrl;
-    const gendernum = userInfo[mentionedUID].gender;
-    let gender = "";
-    const Avatar = (await axios.get(
-      `https://graph.facebook.com/${mentionedUID}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`,
-      { responseType: 'arraybuffer' }
-    )).data;
-if(gendernum == 2){
-gender = "Male";
-}
-    else if(gendernum ==1){
-      gender = "Female";
-    }
-    if(mentionedUID == 100039366339941){
-      gender= "Bading";
-    }
-    fs.writeFileSync('cache/avt.png', Buffer.from(Avatar, 'binary'));
 
-    const formattedMention = {
-      body: `Name: ${fname}\nUID: ${mentionedUID} \n profile URL: ${acculr} \n Gender: ${gender}`,
-      attachment: fs.createReadStream('cache/avt.png')
+module.exports = class {
+  static get config() {
+    return {
+      name: "userinfo",
+      description: "Important Fbbot related infos.",
+      usage: "",
+      credits: "Liane",
+      author: "Liane",
+      category: "Info",
+      usePrefix: false,
+      aliases: [],
+      role: 0,
+      hasPermssion: 0,
+      commandCategory: "Info",
     };
-
-    api.sendMessage(formattedMention, event.threadID);
   }
-}
-          }catch(error){
-            console.log(error);
-          }
-  },////////////////
+  static get metadata() {
+    return this.config;
+  }
+  static get onRun() {
+    return this.run;
+  }
+  static get onStart() {
+    return this.run;
+  }
+  static get run() {
+    return async ({ event, api, args }) => {
+      let ID = event.senderID;
+      if (Object.keys(event.mentions).length > 0) {
+        ID = Object.keys(event.mentions)[0];
+      }
+      if (event.messageReply) {
+        ID = event.messageReply.senderID;
+      }
+      if (args[0] === "raw") {
+        return api.sendMessage(`${ID}`, event.threadID, event.messageID);
+      }
+      if (args[0] === "tid") {
+        return api.sendMessage(`${event.threadID}`, event.threadID, event.messageID);
+      }
+      const { [ID]: info } = await api.getUserInfo(ID);
+      await api.sendMessage(`📛 ${info.name}${info.vanity ? `\n📝 ${info.vanity}` : ""}${info.alternateName ? `\n✨ ${info.alternateName}` : ""}
+${info.gender === 1 ? "👧 Female" : "👦 Male"}
+
+𝙄𝘿: ${ID}
+𝙏𝙄𝘿: ${event.threadID}`, event.threadID, event.messageID);
+    };
+  }
 };
